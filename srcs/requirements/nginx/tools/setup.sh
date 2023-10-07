5bb0b9577e8d8101e673e070763bba9fe6b4af42
Pierrot-jang-pilkyu <jangpilkyu1@gmail.com>
2023-10-08 00:51:19 +0900

    23.10.08-3

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -2,8 +2,9 @@
 # sleep 10
 
 # sed -i 's/index index.html index.htm index.nginx-debian.html index.php;/index index.html index.htm index.php;/gi' /etc/nginx/sites-available/default
-
-# nginx -g daemon off;
+set -e
 
 cat /hosts > /etc/hosts
 rm -rf /hosts
+
+exec "$@"
